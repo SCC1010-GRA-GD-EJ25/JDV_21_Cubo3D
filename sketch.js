b006fc8b69dc22f98807942596f673af70c16fe9
23170373 <158121527+23170373@users.noreply.github.com>
2025-04-08 22:57:06 +0000

    Version 1.0

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
+let imagen
+
 function preload() {
   // put preload code here
+  imagen = loadImage("./images/diamond.jpg")
 }
 
 function setup() {
@@ -13,5 +16,70 @@ function draw() {
   rotateX(frameCount * 0.01)
   rotateY(frameCount * 0.01)
   rotateZ(frameCount * 0.01)
-  box(150)
+  cubo3D(0, 0, 0, 150)
+}
+
+function cubo3D (x, y, z, t){
+  //Textura
+  textureMode(NORMAL)
+  texture(imagen)
+
+  push()
+  translate(x, y, z)
+  
+  //Carra abajo
+  beginShape()
+  vertex(-t, -t, -t,0, 0)
+  vertex(t, -t, -t, 1, 0)
+  vertex(t, t, -t, 1, 1)
+  vertex(-t, t, -t, 0, 1)
+  vertex(-t, -t, -t,0, 0)
+  endShape()
+
+  //Carra arriba
+  beginShape()
+  vertex(-t, -t, t, 0, 0)
+  vertex(-t, t, t, 0, 1)
+  vertex(t, t, t, 1, 1)
+  vertex(t, -t, t, 1, 0)
+  vertex(-t, -t, t, 0, 0)
+  endShape()
+
+  //Carra derecha
+  beginShape()
+  vertex(t, -t, -t, 0, 0)
+  vertex(t, -t, t, 0, 1)
+  vertex(t, t, t, 1, 1)
+  vertex(t, t, -t, 1, 0)
+  vertex(t, -t, -t, 0, 0)
+  endShape()
+
+  //Carra izquierda
+  beginShape()
+  vertex(-t, -t, -t, 0, 0)
+  vertex(-t, t, -t, 0, 1)
+  vertex(-t, t, t, 1, 1)
+  vertex(-t, -t, t, 1, 0)
+  vertex(-t, -t, -t, 0, 0)
+  endShape()
+
+  // Cara delante
+  beginShape()
+  vertex(-t, -t, t, 0, 0) // Esquina inferior izquierda
+  vertex(t, -t, t, 1, 0)  // Esquina inferior derecha
+  vertex(t, t, t, 1, 1)   // Esquina superior derecha
+  vertex(-t, t, t, 0, 1)  // Esquina superior izquierda
+  endShape()
+
+  //Carra detras
+  beginShape()
+  vertex(-t, -t, -t, 0, 0)
+  vertex(-t, -t, t, 0, 1)
+  vertex(t, -t, t, 1, 1)
+  vertex(t, -t, -t, 1, 0)
+  vertex(-t, -t, -t, 0, 0)
+  endShape()
+
+
+  pop()
 }
